f5741c869908e73b471f84e3f2864d74e082c013
Harry <harry@simpleclick.co.uk>
2019-10-16 14:08:31 +0100

    Refactoring Menu
    Initial commit

---
+++ DrinksMachine2/Menu.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinksMachine
+{
+    public class menu
+    {
+        // bool variables for working out what they have ordered
+        
+        public static void Menu()
+        {
+            Program.userInput = Console.ReadLine();
+            int result = Int32.Parse(Program.userInput);
+
+
+
+            
+        }
+ 
+
+
+
+
+
+
+
+
+    }
+}
